50a6c7a09eb420b95730510633e85e3a18cd20c0
Lawrence LCI <lcheunglci@gmail.com>
2022-02-28 00:28:47 -0800

    Add routes and links to the pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { About } from "./page/About";
+import { Articles } from "./page/Articles";
+import { Home } from "./page/Home";
 
 const App = () => {
   return (
-    <h1>Server-side rendering example</h1>
+    <BrowserRouter>
+      <h1>Server-side rendering example</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/articles">Articles</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" exact element={<Home/>} />
+        <Route path="about" element={<About />} />
+        <Route path="articles" element={<Articles />} />
+      </Routes>
+    </BrowserRouter>
+    
   )
 }
 
