de41f9587dde4ae7a4f6ad3e83e6048d74956a22
Lawrence LCI <lcheunglci@gmail.com>
2022-03-08 00:40:07 -0800

    Add code splitting

--- src/page/About.js
+++ src/page/About.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+// import { Three } from './Three';
+// import { Two } from './Two';
+// import { One } from './One';
+
+const One = lazy(() => import('./One'));
+const Two = lazy(() => import('./Two'));
+const Three = lazy(() => import('./Three'));
 
 export const About = () => {
 
     return (
+        <>
         <h1>About us</h1>
+        <Suspense fallback={<p>Loading...</p>}>
+            <One />
+            <Two />
+            <Three />
+        </Suspense>
+        </>
     )
-
 }
---
+++ src/page/One.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const One = () => {
+
+    return (
+        <h2>One</h2>
+    )
+
+}
+
+export default One;
